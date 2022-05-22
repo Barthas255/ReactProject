7749429f5850ce73d4a5b071a235c2de988ff61c
Bartłomiej Jurga <barthas255@gmail.com>
2022-05-22 16:50:27 +0200

    new input feature
    Added Input Form feature

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg'
-import './App.css'
-import MyButton from "./components/HelloReact"
+import React from 'react';
+import './App.css';
+import {InputComponent} from "./components/InputComponent";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello React!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <MyButton />
+            <InputComponent defaultValue={'type your name'}/>
       </header>
     </div>
   )
